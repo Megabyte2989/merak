d331e0ba25ece5481f58b1bd53c9cb34850dd327
kerlous0 <kerlousnasser0@gmail.com>
2024-11-24 20:17:49 +0200

    full main about us + responsive

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import './App.css';
-import Foot from './components/Foot';
-import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Foot from "./components/Foot";
+import Navbar from "./components/Navbar";
+import AboutUs from "./components/AboutUs";
 
 function App() {
-	return (
-		<>
-			<h1></h1>
-			<BrowserRouter>
-				<Navbar />
-				<div className="flex flex-col min-h-screen mx-auto px-4">
-					{/* <ContactUs/> */}
-					{/* <Timer /> */}d
-				</div>
-				<Foot />
-			</BrowserRouter>
-		</>
-	);
+  return (
+    <>
+      <h1></h1>
+      <BrowserRouter>
+        <Navbar />
+        {/* <div className="flex flex-col min-h-screen mx-auto px-4"> */}
+        {/* <ContactUs/> */}
+        {/* <Timer /> */}
+        {/* </div> */}
+        <Routes>
+          <Route path="/aboutUs" element={<AboutUs />} />
+        </Routes>
+        <Foot />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        'dark-purple': 'var(--dark-purple)',
-        'dogwood-rose': 'var(--dogwood-rose)',
-        'russian-violet': 'var(--russian-violet)',
-        'raisin-black': 'var(--raisin-black)',
-        'white': 'var(--white)',
+        "dark-purple": "var(--dark-purple)",
+        "dogwood-rose": "var(--dogwood-rose)",
+        "russian-violet": "var(--russian-violet)",
+        "raisin-black": "var(--raisin-black)",
+        white: "var(--white)",
+        "ghost-white": "var(--ghost-white)",
       },
     },
-    
   },
   plugins: [],
-}
-
+};
