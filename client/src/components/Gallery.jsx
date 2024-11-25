150e898553a5ba3a5b59af8a89e011e888d3d00a
Mayar m.youness <c2200051@cis.edu.eg>
2024-11-25 22:24:19 +0200

    gallery done

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -134,9 +134,8 @@ const Gallery = () => {
           {isAutoPlaying ? "Pause" : "Play"}
         </button>
       </div>
-
-      <div className="bg-white dark:bg-gray-800 h-screen h-full py-6 sm:py-8 lg:py-12 ">
-        <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+      <div className="bg-white dark:bg-gray-800  h-full py-6 sm:py-8 lg:py-12 ">
+        <div className="mx-auto max-w-screen-2xl px-4  ">
           <div className="mb-4 flex items-center justify-between gap-8 sm:mb-8 md:mb-12">
             <div className="flex items-center gap-12">
               <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl dark:text-white">Our Gallery</h2>
